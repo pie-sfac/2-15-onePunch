7fd4fe381673ad8aafdf988863634115d165f9d4
pooh19228 <pooh19228@gmail.com>
2023-08-02 00:49:52 +0900

    로그인 후 다른 페이지로 넘어갈 때 안 됐던 부분 수정

--- src/commons/api/apiLogin.ts
+++ src/commons/api/apiLogin.ts
@@ -7,11 +7,11 @@ import axios from "axios";
 //    return apiBasic;
 // }
 
-const apiBasic = axios.create({
+export const apiBasic = axios.create({
     baseURL: "http://223.130.161.221/api/v1"
 });
 
-export const apiLogin = axios.create({
+ const apiLogin = axios.create({
   baseURL: "http://223.130.161.221/api/v1"
 });
 
@@ -34,9 +34,36 @@ async function getNewTokenWithRefreshToken() {
   }
 }
 
+apiLogin.interceptors.response.use(
+  (response) => response, // 성공한 요청에 대한 응답을 그대로 반환
+  async (error) => {
+    const originalRequest = error.config;
+    
+    // 에러 상태가 401이면 토큰이 만료되었을 수 있음을 확인
+    if (error.response.status === 401 && !originalRequest._retry) {
+      
+      // _retry를 true로 설정하여 무한 재시도 루프에 빠지지 않게 함.
+      originalRequest._retry = true;
+
+      // 새로운 accessToken 가져오기
+      const accessToken = await getNewTokenWithRefreshToken();
+      
+      // 새 토큰을 로컬 스토리지에 저장
+      localStorage.setItem("token", accessToken);
+
+      //원래 요청의 인증 헤더를 업데이트하고 다시 시도
+      originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+      return apiLogin(originalRequest);
+    }
+
+    // 에러가 다른 이유 때문이라면, 다음 핸들러에게 에러를 던진다
+    return Promise.reject(error);
+  }
+);
+
 apiBasic.interceptors.request.use(
     function (config) { 
-      const token = localStorage.getItem("token");
+      const token = localStorage.getItem("tokenForAdmin");
      if (token) {
         config.headers["Authorization"] = `Bearer ${token}`;
       }
